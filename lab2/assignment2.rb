6ee8fda7448064d9c5dcb7f12237db3e711b2384
VelocityZero <brian.bauer@colorado.edu>
2013-09-11 20:57:21 -0700

    initialized files

---
+++ lab2/assignment2.rb
@@ -0,0 +1,42 @@
+#Dessert Class
+class Dessert
+	def initialize(name, calories)
+		@name = name.capitalize
+		@calories = calories
+	end
+
+	def healthy?
+		return @calories < 200
+	end
+	
+	def delicious?
+		return true	
+	end
+end
+
+#Jellybean Class, Extension of Dessert
+class JellyBean < Dessert
+	def initialize(name, calories, flavor)
+		@flavor = flavor
+	end
+
+	def delicious?
+		if "black licorice" == flavor.downcase
+			return false;		
+		end
+		return true
+	end
+end
+
+class Class
+	def attr_accessor_with_history(attr_name)
+		attr_name = attr_name.to_s       # make sure it's a string
+		attr_reader attr_name            # create the attribute's getter
+		attr_reader attr_name+"_history" # create bar_history getter
+		class_eval "def your code here, use %Q for multiline strings"
+	end
+end
+
+class Foo
+    attr_accessor_with_history :bar
+end
